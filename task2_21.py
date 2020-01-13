e9579ebbbfbfbb83159cd943a5c46775cc16c2b8
CoderDast <dastsultan@gmail.com>
2020-01-13 20:33:21 +0600

    task2_21

---
+++ task2_21.py
@@ -0,0 +1,6 @@
+def sum_(sum_1, sum_2, sum_3):
+    return sum_1 + sum_2 + sum_3
+s1 = int(input('First number ='))
+s2 = int(input('Second number ='))
+s3 = int(input('Third number ='))
+print('Result sum of 3 numbers = ',sum_(s1,s2,s3))
